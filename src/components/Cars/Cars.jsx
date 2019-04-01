45240b3163621b2f490c7906480418f26815e23f
Caio Batista <caiobatis7@gmail.com>
2019-04-01 18:38:58 -0300

    static layout

--- src/components/Cars/Cars.jsx
+++ src/components/Cars/Cars.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import SelectItemField from '../SelectItemField/SelectItemField'
 import Grid from '@material-ui/core/Grid'
+import Card from '@material-ui/core/Card'
 
 export default function Cars() {
 
@@ -52,7 +53,6 @@ export default function Cars() {
     }
   ])
 
-
   function handleChangeBrand (e) {
     setBrand(e.target.value)
   }
@@ -69,6 +69,15 @@ export default function Cars() {
     <div className="cars">
       <div className="container">
         <Grid container spacing={16}>
+          <Grid item xs={4}>
+            <h4 className="h4">Dados do veículo:</h4>
+          </Grid>
+          <Grid item xs={4}>
+            <h4 className="h4">Dados do veículo:</h4>
+          </Grid>
+          <Grid item xs={4}>
+            <h4 className="h4">Dados do veículo:</h4>
+          </Grid>
           <Grid item xs={4}>
             <SelectItemField
               value={brand}
@@ -94,11 +103,67 @@ export default function Cars() {
             />
           </Grid>       
         </Grid>
-        <Grid container spacing={16}>
+        <Grid container spacing={16} className="mt50">
           <Grid item xs={12}>
-            {
-              `${brand}, ${model}, ${year}`
-            }
+            <h2 className="h3">Dados do veículo:</h2>
+          </Grid>
+          <Grid item xs={12}>
+            <Card className={'card'}>
+              <div className="vehicle">
+                <div className="image">
+                  <img src="https://via.placeholder.com/150" alt=""/>
+                </div>
+                <div className="data">
+                  <h3 className="h3">VM - Volkswagem</h3>
+                  <p className="p">Amarok 2018 - Prata</p>
+                  <div className="tags">
+                    <div className="category">Diesel</div>
+                    <p className="p">2014</p>
+                    <p className="p">FIPE: 2045644</p>
+                  </div>
+                </div>
+                <div className="price">
+                  <h3 className="h3">R$ 95.500,00</h3>
+                  <p className="p">Mês de ref. Março de 2018</p>
+
+                  <div className="star">
+                    <button className="starButton">Favoritar</button>
+                  </div>
+                </div>
+              </div>
+
+            </Card>
+          </Grid>
+        </Grid>
+      
+        <Grid container spacing={16} className="mt50">
+          <Grid item xs={12}>
+            <h2 className="h3">Veículos favoritados:</h2>
+          </Grid>
+          <Grid item xs={12}>
+            <Card className={'card'}>
+              <div className="table">
+                <div className="titles">
+                  <div className="title">Marca</div>
+                  <div className="title">Modelo</div>
+                  <div className="title">Preço</div>
+                </div>
+                <div className="content">
+                  <div className="line">
+                    <Grid item xs={4}>
+                      Gol
+                    </Grid>
+                    <Grid item xs={4}>
+                      Gol
+                    </Grid>
+                    <Grid item xs={4}>
+                      Gol
+                    </Grid>
+                  </div>
+                </div>
+              </div>
+
+            </Card>
           </Grid>
         </Grid>
       
