0a41c83aeb4047a8cd3569dde9344cd61b454f78
Caio Batista <caiobatis7@gmail.com>
2019-04-01 16:42:13 -0300

    first commit, add project

---
+++ README.md
@@ -0,0 +1,13 @@
+## Getting Started
+
+### 👉🏻 Prerequisites
+
+* [Git]
+* [NPM] or [Yarn]
+
+### 👉🏻 Installing
+
+1. Clone this repository using `git clone`
+2. Onto the this project, install the dependecies and start project using or `yarn install && yarn start`
+
+The website will be available at http://localhost:3000
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default function Header() {
+  return (<>
+    <header>
+      <div className="container">
+        <div className="brand">
+          <h1 className="h1">
+            Laborit
+            <sub>Challenge</sub>
+          </h1>
+        </div>
+      </div>
+    </header>
+  </>)
+}
